1bdd392382d5b3074c52ed66eaf983a90571b805
Jørgen Solli <js@aplia.no>
2021-06-05 16:04:32 +0200

    Fixed hidden navigation

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,8 @@
 <template>
 	<div class="h-full flex flex-col">
-		<div class="relative bg-white overflow-hidden shadow">
-			<div class="max-w-7xl mx-auto">
-				<div
-					class="relative z-10 lg:max-w-2xl lg:w-full"
-				>
-					<div class="relative pb-4 pt-6 px-4 sm:px-6 lg:px-8">
-						<nav
-							id="navigation"
-							class="relative flex items-center justify-between sm:h-10 lg:justify-start"
-							aria-label="Global"
-						>
-							<div
-								class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0"
-							>
-								<div
-									class="flex items-center justify-between w-full md:w-auto"
-								>
-									<RouterLink to="/">
-										<span class="sr-only">Drizzle</span>
-										<img
-											class="h-8 w-auto sm:h-10"
-											src="@/assets/logo.png"
-										/>
-									</RouterLink>
-								</div>
-							</div>
-							<div
-								class="hidden md:block md:ml-10 md:pr-4 md:space-x-8"
-							>
-								<RouterLink
-									to="/"
-									class="menu-item font-medium"
-								>
-									Drizzle
-								</RouterLink>
-
-								<RouterLink
-									to="/about"
-									class="menu-item font-medium"
-								>
-									About
-								</RouterLink>
-							</div>
-						</nav>
-					</div>
-				</div>
-			</div>
-		</div>
-
 		<transition name="fade" mode="out-in">
-			<main class="">
+			<main>
+				<Navigation />
 				<RouterView />
 			</main>
 		</transition>
@@ -61,6 +13,7 @@
 
 <script>
 import Footer from '@/components/Footer';
+import Navigation from '@/components/Navigation';
 
 export default {
 	data() {
@@ -70,6 +23,7 @@ export default {
 	},
 	components: {
 		Footer,
+		Navigation,
 	}
 };
 </script>
